770dcbdbd8569eb26140f7dba9d329da41c5cfca
Nurlat Bekdullayev <nurlat.bekdullayev@syngenta.com>
2026-04-10 14:01:38 +0500

    Update api url for client

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ interface AppConfig {
 }
 
 const config: AppConfig = {
-  apiUrl: import.meta.env.VITE_API_URL || "http://localhost:8080",
+  apiUrl: import.meta.env.VITE_API_URL ?? "http://localhost:8080",
   backgroundMusicVolume: import.meta.env.VITE_BACKGROUND_MUSIC_VOLUME || 0.05,
   weddingDate: import.meta.env.VITE_WEDDING_DATE || "2026-07-04",
   adminStorageKey:
