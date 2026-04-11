a9f5c70649bb26756244a43d0b081b9c2361ef2a
Nurlat Bekdullayev <nurlat.bekdullayev@syngenta.com>
2026-04-11 21:34:58 +0500

    Add mute button, fix bug when media item close button gets rendered outside if view port, change routing to browser router, so admin is in /admin

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import AdminPage from "@/pages/AdminPage";
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />} />
       <Route path="/admin" element={<AdminPage />} />
+      <Route path="/" element={<LandingPage />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -2,6 +2,7 @@ import RSVPTable from "@/components/admin/rsvp-table/RSVPTable";
 import LoginForm from "@/components/admin/login/LoginForm";
 import { useAuth } from "@/hooks/useAuth";
 import styles from "@/pages/AdminPage.module.css";
+import { Link } from "react-router-dom";
 
 export default function AdminPage() {
   const { authenticated, loading, error, login, logout } = useAuth();
@@ -15,7 +16,13 @@ export default function AdminPage() {
   return (
     <div className={styles.adminWrapper}>
       <div className={styles.adminHeader}>
-        <h2 className={styles.adminTitle}>Admin</h2>
+        <div className={styles.leftHeaderSection}>
+          <h2 className={styles.adminTitle}>Admin</h2>
+        </div>
+
+        <Link to="/" className={styles.logoutBtn}>
+          Back to Site
+        </Link>
         <button className={styles.logoutBtn} onClick={logout}>
           Exit
         </button>
