5a1ce435baf03c66e0c6ab10f5aa301cfb5e35bd
Nurlat Bekdullayev <nurlat.bekdullayev@syngenta.com>
2026-04-11 22:16:21 +0500

    Remove support for older browsers

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: "esnext",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
